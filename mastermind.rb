2dea94cc569232758ab4ff117ea4c3b558b1d0e3
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-09-15 13:28:29 -0500

    Add method that creates a random, 4 item array from the symbols available for gameplay

--- mastermind.rb
+++ mastermind.rb
@@ -9,3 +9,15 @@
 # 5. If player doesn't get the answer in 8 turns, computer reveals code
 # 6. Points start at 8 and are reduced each round 
 
+$symbol_options = ['!','@','#','$','%','&','*']
+
+def computer_codemaster
+  $computer_code = []
+  4.times do 
+    $computer_code += [$symbol_options.sample]
+  end 
+  $computer_code
+end 
+
+computer_codemaster()
+  
