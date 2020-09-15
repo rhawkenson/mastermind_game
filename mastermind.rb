0a778b5a7c64962a6430187c6dfc2c207d1a5022
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-09-15 13:11:07 -0500

    Define subproblems to solve in game creation

---
+++ mastermind.rb
@@ -0,0 +1,11 @@
+# SubProblems 
+# 1. Computer chooses a random order 
+# 2. Player makes a guess
+# 3. Computer sends feedback on player guess
+#     'x' for exact answer 
+#     'o' for right color, wrong position 
+#     Return value is printed every round, updated each time
+# 4. If player gets the answer, announce winner 
+# 5. If player doesn't get the answer in 8 turns, computer reveals code
+# 6. Points start at 8 and are reduced each round 
+
