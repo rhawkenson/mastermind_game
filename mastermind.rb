bfb4be5be1beda3a6652a3ca4a7d3e5ca7e54549
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-09-15 14:19:50 -0500

    Add method that prompts player for their guess, then evaluates the guess for correct length and characters

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,8 @@
 # SubProblems 
 # 1. Computer chooses a random order 
 # 2. Player makes a guess
+#     Evaluate if the guess is valid: Corrent length, correct symbols
+#     Evaluate if the guess matches the computer's code 
 # 3. Computer sends feedback on player guess
 #     'x' for exact answer 
 #     'o' for right color, wrong position 
@@ -11,6 +13,7 @@
 
 $symbol_options = ['!','@','#','$','%','&','*']
 
+#Computer randomly selects 3 symbols from the $symbol_options (repeats are allowed)
 def computer_codemaster
   $computer_code = []
   4.times do 
@@ -19,5 +22,27 @@ def computer_codemaster
   $computer_code
 end 
 
-computer_codemaster()
-  
+# Player is prompted to enter their guess, which is converted from a string to an array
+def player_guess 
+  puts "Symbols: ! @ # $ % & *"
+  puts "Enter your guess:"
+  get_guess = gets.chomp
+  guess = get_guess.split(//)
+  player_codeguesser(guess)
+
+end 
+
+#The player's guess is evaluated - must be correct length and only contains $symbol_options
+def guess_evaluation(guess)
+  if guess.length == 4
+    if guess.all?{|symbol| $symbol_options.include?(symbol)}
+      puts 'valid entry'
+    end 
+  else 
+   puts "\n\nINVALID CODE. Please re-enter your code. 
+You must enter 4 symbols and may ONLY use the following:"
+    guess_evaluation()
+  end 
+end 
+
+player_guess()
