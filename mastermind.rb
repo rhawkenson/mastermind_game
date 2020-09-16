68d6130ac69764a5fe0d4e04356ef3bd93253b84
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-09-16 14:48:25 -0500

    Add conditional statement to code_broken that will get another guess, from with either the computer or the player, accordingly

--- mastermind.rb
+++ mastermind.rb
@@ -26,7 +26,7 @@ class Game
 puts "an unbreakable code. If you choose to create the code, you will make a 4-character code using only "
 puts "the following symbols: #{$symbol_options}, and the computer will have "
 puts "8 turns to crack the code. If you choose to break the code, the computer will create a secret " 
-puts "code, using the same symbols,and you will have 8 turns to break the code. "
+puts "code, using the same symbols, and you will have 8 turns to break the code. "
     puts "\n\nWould you like to create the code or break the code?\n" 
     
     self.start
@@ -39,8 +39,10 @@ puts "code, using the same symbols,and you will have 8 turns to break the code. 
     role = user_choice.downcase
     
     if role == "create"
-      Codebreaker.player_codemaster
+      computer = Codebreaker.new('computer')
+      #player_codemaster
     elsif role == "break"
+      Codemaster.new
       Codemaster.computer_codemaster    
     else 
       puts "\nPlease enter a valid choice."
@@ -49,7 +51,9 @@ puts "code, using the same symbols,and you will have 8 turns to break the code. 
   end 
 end 
 
+
 #Class names refer to the role of the computer. If the player chooses to break the code, the computer is the 'Codemaster'
+
 class Codemaster
 #Computer randomly selects 3 symbols from the $symbol_options (repeats are allowed)
   def self.computer_codemaster
@@ -58,13 +62,29 @@ class Codemaster
       $computer_code += [$symbol_options.sample]
     end 
     $computer_code
-    Codebreaker.player_guess
+    self.continue()
+  end 
+  def self.continue
+    Codebreaker.new('player')
   end 
 end
 
 #Class names refer to the role of the computer. If the player chooses to create the code, the computer is the 'Codebreaker'
-class Codebreaker
-  def self.player_codemaster
+class Codebreaker 
+  def initialize(name)
+    @name = name
+
+    if @name == 'player'
+      player_guess
+
+    elsif @name == 'computer'
+      player_codemaster
+    else
+      puts 'this shit dont work'
+    end 
+  end
+
+  def player_codemaster
     puts "Symbols: ! @ # $ % ^ & * ?"
     puts "Please select your secret code:"
     code = gets.chomp.split(//)
@@ -82,24 +102,25 @@ class Codebreaker
   You must enter 4 symbols and may ONLY use the following:"
       self.player_codemaster()
     end 
+    computer_guess()
   end
 
 
 # Player is prompted to enter their guess, which is converted from a string to an array
-  def self.player_guess 
+  def player_guess 
     if $rounds == 8
       puts "GAME OVER. The code you were trying to break was #{$computer_code}. You have earned 0 points."
     
     else 
     puts "Symbols: ! @ # $ % ^ & * ?"
     puts "Enter your guess:"
-    get_guess = gets.chomp
-    guess = get_guess.split(//)
-    self.guess_evaluation(guess)
+    $guess = gets.chomp.split(//)
+    guess_evaluation($guess)
     end
   end 
 
-  def self.computer_guess
+#Random computer guess is generated
+  def computer_guess
     if $rounds == 8
       puts "GAME OVER. The computer was not able to break your code!"
     
@@ -108,29 +129,30 @@ class Codebreaker
       4.times do 
         comp_guess += [$symbol_options.sample]
       end 
-    self.code_check(comp_guess, $player_code)
+      puts "Computer guessed: #{comp_guess.join}"
+      code_check(comp_guess, $player_code)
     end
   end 
 
 #The player's guess is evaluated - must be correct length and only contains $symbol_options
-  def self.guess_evaluation(guess)
+  def guess_evaluation(guess)
     if guess.length == 4
       if guess.all?{|symbol| $symbol_options.include?(symbol)}
-        self.code_check(guess, $computer_code)
+        code_check($guess, $computer_code)
       else 
         puts "\n\nINVALID CODE. Please re-enter your code. 
     You must enter 4 symbols and may ONLY use the following:"
-        self.player_guess()
+        player_guess()
       end 
     else 
     puts "\n\nINVALID CODE. Please re-enter your code. 
   You must enter 4 symbols and may ONLY use the following:"
-      self.player_guess()
+      player_guess()
     end 
   end 
 
 # The Player's guess is compared to the computer's code; 'x' is return for exactly correcty symbols, 'o' is returned for symbols that are in the code but not in the position they are currently in, and '_' is return for symbols that are not in the code at all 
-  def self.code_check(guess, code)
+  def code_check(guess, code)
     $rounds += 1
     $points -= 1 
     x = 0
@@ -144,21 +166,41 @@ class Codebreaker
         feedback += ['_']
       end
       x += 1
-      feedback
     end 
-    print feedback.join
-    self.code_broken(guess, code)
+    puts feedback.join
+    code_broken(guess, code, feedback)
   end 
 
 
 # If the guess perfectly matches the computer's code, the game ends 
-  def self.code_broken(guess, code)
-    if code == guess
-      puts "\nYOU WON!"
-      puts "You have earned #{$points} points\n\n"
+  def code_broken(guess, code, feedback)
+    # puts "Code: #{code}"
+    # puts "Player's Secret Code: #{$player_code}"
+    # puts "Guess: #{guess}"
+    # puts "Feedback: #{feedback}"
+
+# If the computer is the codemaster, either end the game or keep getting the player's guesses
+    if code == $computer_code  
+      if code == guess
+        puts "\nYOU WON!"
+        puts "You have earned #{$points} points\n\n"
+      else 
+        print "\n\n code is computer code"
+        player_guess()
+      end 
+#If the computer is the code guesser, either end the game or keep getting the player's guesses
+    elsif code == $player_code
+      puts "Press enter to continue"
+      gets.chomp
+      if code == guess
+        puts "\nCOMPUTER WON!"
+        puts "Computer earned #{$points} points\n\n"
+      else 
+        print "\n\n code is player code \n\n"
+        self.computer_guess()
+      end 
     else 
-      print "\n\n"
-      self.player_guess()
+      puts "the secret code is not working"
     end 
   end 
 end 
